a72827a17a5f6df4e4bfc3c9e8a28d777af0e765
jcmonterrosas <jcmonterrosas@unal.edu.co>
2020-04-12 20:05:16 -0500

    :pencil::wrench: Requirements and names

--- ConfigAccount/ConfigAccount/settings.py
+++ ConfigAccount/ConfigAccount/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'ConfigAccount.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'pinart-config.account-db.sqlite3'),
     }
 }
 
