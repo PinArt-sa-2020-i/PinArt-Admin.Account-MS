a481ebee641a09747ad39f7392f3c8b797c711c3
Juan Monterrosa Sanchez <38740763+jcmonterrosas@users.noreply.github.com>
2020-05-02 19:38:51 -0500

    Develop (#3)
    * Update views.py
    
    * :sparkles::recycle: Add get_by_user and modified Post in Sessions
    
    * :sparkles::boom: Add create Token with session
    
    * :wrench: settings host to correct db

--- ConfigAccount/Account/views.py
+++ ConfigAccount/Account/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from jwcrypto import jwt, jwk
 from .models import TipoNotificacion, ConfigNotificaciones, Medio, Sesiones, CuentasEnlazadas
 from .serializers import TipoNotificacionSerializer, ConfigNotificacionesSerializer, MedioSerializer, SesionesSerializer, CuentasEnlazadasSerializer
+import json
 
 class TipoNotificacionViewSet(viewsets.ModelViewSet):
     serializer_class = TipoNotificacionSerializer
@@ -18,6 +22,46 @@ class MedioViewSet(viewsets.ModelViewSet):
 class SesionesViewSet(viewsets.ModelViewSet):
     serializer_class = SesionesSerializer
     queryset = Sesiones.objects.all()
+
+    @action(detail=False, methods=['get'])
+    def get_by_user(self, request):
+        key = jwk.JWK(kty = 'oct', k = 'pinart20')
+        key.export()
+        token = request.headers['auth']
+        st = jwt.JWT(key = key, jwt = token)
+        result = json.loads(st.claims)
+        sessionID = int(result['sessionID'])
+        session = get_object_or_404(self.queryset, pk = sessionID)
+        serializer = SesionesSerializer(session)
+        if serializer.data['Activo']:
+            return Response(serializer.data['IdUsuario'])
+        else:
+            error = {
+                'message' : 'Sesion Inactiva'
+            }
+            return Response(error, status = status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request):
+        serializer = SesionesSerializer(data = request.data)
+        key = jwk.JWK(kty = 'oct', k = 'pinart20')
+        key.export()
+        if serializer.is_valid():
+            serializer.save()
+            sessionid = serializer.data['id']
+            userid = serializer.data['IdUsuario']
+            user = {
+                'userID' : userid,
+                'sessionID' : sessionid
+            }
+            Token = jwt.JWT(header = {'alg' : 'HS256'}, claims = user)
+            Token.make_signed_token(key)
+            result = Token.serialize()
+            response = {
+                'data' : serializer.data,
+                'Token' : result  
+            }
+            return Response(response, status = status.HTTP_201_CREATED)
+        return Response(serializer.data, status = status.HTTP_400_BAD_REQUEST)
 
 class CuentasEnlazadasViewSet(viewsets.ModelViewSet):
     serializer_class = CuentasEnlazadasSerializer
